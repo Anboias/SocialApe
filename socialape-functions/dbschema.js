201e9c8b0d9e20f6d8ebc32615d71ad7d1baa4f3
Anboias <biasinovschi@gmail.com>
2020-06-13 13:43:50 +0300

    initial

---
+++ socialape-functions/dbschema.js
@@ -0,0 +1,55 @@
+let db = {
+    users: [
+        {
+            userId: 'NxE0B5EfNUSFhPHuJVORvLMqz3m1',
+            email: 'user@email.com',
+            handle: 'user',
+            createdAt: '2020-06-10T20:22:48.962Z',
+            imageUrl: 'https://firebasestorage.googleapis.com/v0/b/socialape-f51c8.appspot.com/o/108672281654.jpg?alt=media',
+            bio: 'Hello my name is user nice to meet you',
+            website: 'https://user.com',
+            location: 'Cluj, RO'
+        }
+    ],
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'This is the scream body',
+            createdAt: '2020-06-10T19:14:12.677Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ],
+    comments: [
+        {        
+            userHandle: 'user',
+            screamId: 'p5HC87QixK1Z2s7rkihE',
+            body: 'nice one mate!',
+            createdAt: '2020-06-10T20:22:48.962Z'
+        }
+    ]
+};
+
+const userDetails = {
+    // Redux data
+    credentials: {
+        userId: 'NxE0B5EfNUSFhPHuJVORvLMqz3m1',
+        email: 'user@email.com',
+        handle: 'user',
+        createdAt: '2020-06-10T20:22:48.962Z',
+        imageUrl: 'https://firebasestorage.googleapis.com/v0/b/socialape-f51c8.appspot.com/o/108672281654.jpg?alt=media',
+        bio: 'Hello my name is user nice to meet you',
+        website: 'https://user.com',
+        location: 'Cluj, RO'
+    },
+    likes: [
+        {
+            userHandle: 'user',
+            screamId: 'p5HC87QixK1Z2s7rkihE'
+        },
+        {
+            userHandle: 'user',
+            screamId: 'EFd754G9toWnUJEdd9xR'
+        },
+    ]
+}
---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyDFx8VGMs7qvY-DipjRN8g9_ikwJvIvlyA",
+    authDomain: "socialape-f51c8.firebaseapp.com",
+    databaseURL: "https://socialape-f51c8.firebaseio.com",
+    projectId: "socialape-f51c8",
+    storageBucket: "socialape-f51c8.appspot.com",
+    messagingSenderId: "376218795821",
+    appId: "1:376218795821:web:c607a6c207f30e367d5da4",
+    measurementId: "G-HFRY3KWPCJ"
+};
